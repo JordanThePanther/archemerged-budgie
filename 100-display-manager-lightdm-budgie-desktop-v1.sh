96027d4dfb318566b8d5f40e2d9a832a6a005511
JordanThePanther <jamoore@uwm.edu>
2018-03-09 23:51:57 -0600

    Added Lightdm and Budgie installation script

---
+++ 100-display-manager-lightdm-budgie-desktop-v1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+sudo pacman -Syyu
+sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed
+sudo pacman -S gnome --noconfirm --needed
+sudo pacman -S budgie-desktop
+sudo systemctl enable lightdm.service -f
+sudo systemctl set-default graphical.target
+
+echo "Remove anything you do not like from the installed applications"
+
+sudo pacman -R gnome-backgrounds --noconfirm
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
